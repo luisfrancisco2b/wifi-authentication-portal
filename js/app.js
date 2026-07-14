eae17f85077ce4c47fa948145da73273c3f8ba2a
Luis Francisco <183943582+luisfrancisco2b@users.noreply.github.com.>
2026-07-14 19:13:11 -0300

    feat: add password visibility toggle using emoji

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 // 1 - Select the form element using querySelector
 const form = document.querySelector("form");
+const inputMatricula = document.querySelector("#matricula");
+const inputCpf = document.querySelector("#cpf");
+const btnToggleSenha = document.querySelector("#btn-toggle-senha");
 
 // 2 - Add an event listener for the form submission
 form.addEventListener("submit", function (e) {
-  // Select the input fields by their IDs
-  const inputMatricula = document.querySelector("#matricula");
-  const inputCpf = document.querySelector("#cpf");
-
   // Basic validation: check if either fields is empty (ignoring whitespacing)
   if (inputMatricula.value.trim() === "" || inputCpf.value.trim() === "") {
     // Prevent the form from submitting and reloading the page
@@ -14,5 +13,16 @@ form.addEventListener("submit", function (e) {
 
     // Display a warning message to the user
     alert("Por favor, preencha os campos de Matrícula e CPF para se conectar.");
+  }
+});
+
+// 3 -
+btnToggleSenha.addEventListener("click", function () {
+  if (inputCpf.type === "password") {
+    inputCpf.type = "text";
+    btnToggleSenha.textContent = "🙈";
+  } else {
+    inputCpf.type = "pasword";
+    btnToggleSenha.textContent = "👁️";
   }
 });
